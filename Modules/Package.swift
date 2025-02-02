88d517cce5a7fa129454103b28a6c34e55da098c
Oleksandr Balabanov <bbalabanovv@gmail.com>
2025-02-02 10:13:38 +0000

    previews do work with this module structure

--- Modules/Package.swift
+++ Modules/Package.swift
@@ -345,6 +345,7 @@ let package = Package(
                 "Module14",
                 "Module15",
                 "Module16",
+                "Module60",
             ],
             swiftSettings: .common
         ),
